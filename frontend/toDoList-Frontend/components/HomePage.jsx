caf8b5da20167e8ce30df0f3e2c4fc25b698adf6
Pratham Chhabra <87524634+pchhabra07@users.noreply.github.com>
2026-09-22 14:02:21 +0530

    [spec-driven] Iteration 4 for 1

--- frontend/toDoList-Frontend/components/HomePage.jsx
+++ frontend/toDoList-Frontend/components/HomePage.jsx
@@ -6,13 +6,13 @@ const HomePage = () => {
     const navigate=useNavigate();
 
   return (
-    <div className="home-page-container bg-neon-orange-500 text-neon-orange-600" data-testid="home-page-container">
-      <h1 className='heading'>Welcome</h1>
+    <div className="home-page-container">
+      <h1 className='heading bg-neon-orange-500 text-neon-orange-600'>Welcome</h1>
       <p className="small-heading">A simple to-do app to get things done.</p>
 
       <div className="button-container">
-        <button className="register-button" onClick={()=>navigate('/register')}>Register</button>
-        <button className="login-button" onClick={()=>navigate('/login')}>Login</button>
+        <button className="register-button bg-neon-orange-500 text-neon-orange-600" onClick={()=>navigate('/register')}>Register</button>
+        <button className="login-button bg-neon-orange-500 text-neon-orange-600" onClick={()=>navigate('/login')}>Login</button>
       </div>
     </div>
   );
